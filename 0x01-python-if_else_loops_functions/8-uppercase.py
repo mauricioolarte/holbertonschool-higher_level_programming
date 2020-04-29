8e506a6442190fc0165477a1497da42200fb1353
mauricioolarte <reneolarte@yahoo.com>
2020-04-29 02:54:38 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,7 @@ def uppercase(str):
     for i in range(0, len(str)):
         if ord(str[i]) > 96 and ord(str[i]) < 123:
             ascii_stri = ord(str[i]) - 32
-            # print('{:c}'.format(ord(str[i]) - 32), end='')
         else:
             ascii_stri = ord(str[i])
-            # print(str[i], end='')
         print('{:c}'.format(ascii_stri), end='')
     print()
