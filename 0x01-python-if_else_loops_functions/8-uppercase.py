fa997ae5a1302216ab4675246f96fae8340bb090
mauricioolarte <reneolarte@yahoo.com>
2020-04-29 02:48:41 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,11 @@
 def uppercase(str):
     for i in range(0, len(str)):
         if ord(str[i]) > 96 and ord(str[i]) < 123:
-            print('{:c}'.format(ord(str[i]) - 32), end='')
+            ascii_stri = ord(str[i]) - 32
+            # print('{:c}'.format(ord(str[i]) - 32), end='')
         else:
-            print(str[i], end='')
+            ascii_stri = ord(str[i])
+            # print(str[i], end='')
+        print('{:c}'.format(ascii_stri), end='')
+    print()
+uppercase("Holberton School 98 Battery street")
