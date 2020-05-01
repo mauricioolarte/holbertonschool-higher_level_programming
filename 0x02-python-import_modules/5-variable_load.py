939533b042a6c3b44e046a1b045006e699c5ac2b
mauricioolarte <reneolarte@yahoo.com>
2020-05-01 04:06:08 +0000

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def main():
+    names = variable_load_5.a
+    print(names)
+if __name__ == '__main__':
+    import sys
+    import variable_load_5
+    import builtins
+    main()
