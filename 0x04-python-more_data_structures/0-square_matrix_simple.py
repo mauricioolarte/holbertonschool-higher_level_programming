7520448947edd809ef47a8571c30f5da65c140b9
mauricioolarte <reneolarte@yahoo.com>
2020-05-08 05:03:47 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[column[i] for column in matrix] for i in range(len(matrix))]
-    new_matrix2 = [[row[i] for row in matrix] for i in range(len(matrix))]
-    # for i in range(len(matrix)):
-    #     print(len(matrix[i]))
-
-    # new_matrix = []
-    # print(len(matrix))
-    # for i in range(len(matrix)):
-    #     new_matrix[i] = []
-    #     for j in range(len(matrix[i])):
-    #         new_matrix[j].append(matrix[i][j] ** 2)
+    new_matrix = []
+    for i in range(len(matrix)):
+        file = []
+        for j in range(len(matrix[i])):
+            file.append(matrix[i][j] ** 2)
+        new_matrix.append(file)
     return (new_matrix)
