d3eb9861ad9b84fccdb20471f15c34c635f7c4d9
mauricioolarte <reneolarte@yahoo.com>
2020-05-08 01:38:14 +0000

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in range(0, len(my_list)):
+        if my_list[i] == search:
+            new_list.insert(i, replace)
+        else:
+            new_list.insert(i, my_list[i])
+    return (new_list)
