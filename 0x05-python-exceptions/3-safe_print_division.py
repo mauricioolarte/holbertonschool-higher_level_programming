16eb1b29c402a63b156998de9f32c95f86019f82
mauricioolarte <reneolarte@yahoo.com>
2020-05-18 19:15:54 +0000

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        return(result)
+    except ZeroDivisionError:
+        result = "None"
+        return(result)
+    finally:
+        print("Inside result: {}".format(result))
