015f423ce59fc3606340c03c0e0ff8a44c6ae76d
mauricioolarte <reneolarte@yahoo.com>
2020-05-18 21:38:40 +0000

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,5 +17,6 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list[i] = 0
             mens = "out of range"
         finally:
-            print(mens)
+            if (mens != ""):
+                print(mens)
     return(new_list)
