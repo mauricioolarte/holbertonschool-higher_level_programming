7003e00162b6c2a8cb8b42ef61c2854a9b986859
mauricioolarte <reneolarte@yahoo.com>
2020-05-20 06:13:56 +0000

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -112,7 +112,7 @@ class Square():
         """
         if self.size == 0:
                 print()
-        else:        
+        else:
             for coordenadesx in range(self.position[1]):
                 print()
             for i in range(self.size):
