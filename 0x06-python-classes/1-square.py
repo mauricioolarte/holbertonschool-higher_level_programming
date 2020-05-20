a115288f6379a43f03144003ab0718af2185c500
mauricioolarte <reneolarte@yahoo.com>
2020-05-20 00:42:11 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+""" this is for init a class square"""
 class Square():
+    """this class is a square
+    Args: None
+    Attributes: 
+        size (int):is a integer.
+    """
     def __init__(self, size=0):
+        """ this for init fuction
+        Args:
+            size(int)
+        """
         self.__size = size
