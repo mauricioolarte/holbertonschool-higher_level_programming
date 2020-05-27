384632811b678b92bd6913fa0b2d0415d9d4cbfd
mauricioolarte <reneolarte@yahoo.com>
2020-05-27 04:10:21 +0000

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" this module is for a class that loked a instances names"""
+
+
 class LockedClass:
+    """ class whit locked names of instances"""
     __slots__ = 'first_name'
