afe9af9ab10fe0f409d4566819dcd9f717f20684
mauricioolarte <reneolarte@yahoo.com>
2020-06-03 01:26:58 +0000

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """ this module have a fuction tha evalue if a obje is intance of class"""
 
 
-def def is_kind_of_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """return a tru if objecs is part of class"""
     if isinstance(obj, a_class):
         return (True)
