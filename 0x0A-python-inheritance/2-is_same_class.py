e97b54289189e0df771829d9ecb0702fbcac3d71
mauricioolarte <reneolarte@yahoo.com>
2020-06-03 01:17:52 +0000

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module have a fuction tha evalue if a obje is intance of class"""
+
+
+def is_same_class(obj, a_class):
+    """return a tru if objecs is part of class"""
+    if isinstance(obj, a_class):
+        return (True)
+    return(False)
+
+      
