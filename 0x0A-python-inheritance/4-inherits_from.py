65dfc191d2e5d8de7b257a4d03f52814b9ec790a
mauricioolarte <reneolarte@yahoo.com>
2020-06-03 02:10:16 +0000

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,8 +2,8 @@
 """ this module have a fuction tha evalue if a obje is intance of class"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """return a tru if objecs is part of class"""
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return (True)
     return(False)
