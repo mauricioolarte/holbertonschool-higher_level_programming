bc0845869c56b9729ca04ad33660e17a915f3f83
mauricioolarte <reneolarte@yahoo.com>
2020-06-04 02:06:57 +0000

    7-save_to_json_file.py

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this file is for comvert files to json"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    string = json.dumps(my_obj)
+    with open(filename, 'w') as f:
+        f.write(string)
