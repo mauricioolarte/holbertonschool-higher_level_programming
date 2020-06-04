3c5647ab7c8161639c9e78ab3cc2c79b837cf6f3
mauricioolarte <reneolarte@yahoo.com>
2020-06-04 01:45:33 +0000

    5-to_json_string.py

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this file is for comvert files to json"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ this file is for comvert files to json"""
+    return(json.dumps(my_obj))
