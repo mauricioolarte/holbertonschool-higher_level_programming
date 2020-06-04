b3116a0289c1609ecdaab313bd269f25c8b3241f
mauricioolarte <reneolarte@yahoo.com>
2020-06-04 01:52:30 +0000

    6-from_json_string.py

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this file is for comvert files to json"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """ this file is for comvert files to json"""
+    return(json.loads(my_str))
