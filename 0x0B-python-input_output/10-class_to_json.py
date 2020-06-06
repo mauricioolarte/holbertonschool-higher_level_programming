b2a63a11369027bdc388060fef8435a3d67d4c5e
mauricioolarte <reneolarte@yahoo.com>
2020-06-06 03:01:42 +0000

    10-class_to_json.py

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -2,7 +2,7 @@
 """ this file is for comvert files to json"""
 
 import json
-
+""" this file is for comvert files to json"""
 
 def class_to_json(obj):
     """ this file is for comvert files to json"""
