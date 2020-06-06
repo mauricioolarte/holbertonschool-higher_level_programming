2101edf7401bc48672363615bf04badb073319bd
mauricioolarte <reneolarte@yahoo.com>
2020-06-06 03:29:07 +0000

    10-class_to_json.py

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ this file is for comvert files to json"""
 
-import json
-""" this file is for comvert files to json"""
-
 def class_to_json(obj):
     """ this file is for comvert files to json"""
-    return(json.dumps(obj.__dict__))
+    a = str(obj)
+    return(a)
