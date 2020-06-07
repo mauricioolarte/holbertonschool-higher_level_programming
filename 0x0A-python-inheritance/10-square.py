4a4869d7ba43062af68eb18414b8c207caca5208
mauricioolarte <reneolarte@yahoo.com>
2020-06-07 03:46:49 +0000

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,6 +9,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if type(value) != int:
+            print(type(value))
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
