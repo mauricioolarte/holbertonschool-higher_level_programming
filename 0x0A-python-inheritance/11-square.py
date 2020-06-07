2884a4ef90927a6c47c2b9a1db6149681a0c95f7
mauricioolarte <reneolarte@yahoo.com>
2020-06-07 04:24:40 +0000

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,15 +15,32 @@ class BaseGeometry:
         return True
 
 
-class Square(BaseGeometry):
+class Rectangle(BaseGeometry):
     """empty class"""
-    def __init__(self, width):
+    def __init__(self, width, height):
         if self.integer_validator("width", width):
             self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
 
     def area(self):
         a = self.__width
+        b = self.__height
+        return a * b
+
+    def __str__(self):
+        return ('[Rectangle] {}/{}'.format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """empty class"""
+    def __init__(self, size):
+        if self.integer_validator("size", size):
+            self.__size = size
+
+    def area(self):
+        a = self.__size
         return a * a
 
     def __str__(self):
-        return ('[Square] {}/{}'.format(self.__width, self.__width))
+        return ('[Square] {}/{}'.format(self.__size, self.__size))
