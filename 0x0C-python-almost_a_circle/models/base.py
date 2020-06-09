90a21837a4660a5a198e7be38f3c23cbad246527
mauricioolarte <reneolarte@yahoo.com>
2020-06-09 22:38:10 +0000

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,6 +48,6 @@ class Base():
             Return : a json sting
         """
         if json_string is None:
-            return ("[]")
+            return ([])
         else:
             return json.loads(json_string)
