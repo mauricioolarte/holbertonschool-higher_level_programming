b7cd573b24f02446e392fab9444314a93b725b80
mauricioolarte <reneolarte@yahoo.com>
2020-06-09 21:24:54 +0000

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""doc"""
+"""this module if for a class base
+Attributes:
+            def __init__: constructor method
+"""
 
 
-from base import Base
-from rectangle import Rectangle
+from models.base import Base
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """doc"""
+    """ this a base class
+    Attributes:
+    __init__: construct method
+    to_json_string(list_dictionaries): conver dir in json file.
+    def from_json_string(json_string): import json file.
+    """
     def __init__(self, size, x=0, y=0, id=None):
+        """ constructor method
+        Args:
+            width: is the number of the subslass.
+            height:
+            x, y:
+            id:
+        """
         Rectangle.width.__set__(self, size)
         Rectangle.height.__set__(self, size)
         Rectangle.x.__set__(self, x)
@@ -16,14 +31,20 @@ class Square(Rectangle):
         Base.__init__(self, id)
 
     def __str__(self):
-        """doc"""
+        """this method take a list and return a json string
+            Argument : None
+            Return : Area of rectangle
+        """
         return(
             '[Square] ({}) {}/{} - {}'.format(
                 self.id, self.x, self.y, self.width)
             )
 
     def update(self, *args, **kwargs):
-        """doc"""
+        """this method take a list and return a json string
+            Argument : None
+            Return : Area of rectangle
+        """
         if args and len(args) > 0:
             for i in range(len(args)):
                 if i == 0:
@@ -46,7 +67,10 @@ class Square(Rectangle):
                     Rectangle.y.__set__(self, value)
 
     def to_dictionary(self):
-        """doc"""
+        """this method take a list and return a json string
+            Argument : None
+            Return : Area of rectangle
+        """
         list_dic = {}
         list_dic['id'] = self.id
         list_dic['size'] = self.width
@@ -56,12 +80,18 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        """doc"""
+        """this method take a list and return a json string
+            Argument : None
+            Return : Area of rectangle
+        """
         return self.width
 
     @size.setter
     def size(self, size):
-        """doc"""
+        """this method take a list and return a json string
+            Argument : None
+            Return : Area of rectangle
+        """
         if type(size) is not int:
             raise TypeError('width must be and integer')
         elif size <= 0:
