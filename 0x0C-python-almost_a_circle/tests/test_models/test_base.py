3d7e7e8b95dadc842432b0762d8dc6916249bc20
mauricioolarte <reneolarte@yahoo.com>
2020-06-10 16:13:08 +0000

    tests/

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,30 +6,40 @@
 
 
 import unittest
+ import pep8
 from models.base import Base
 
 
-class TestBaseMethods(unittest.TestCase):
+class Test_BaseMethods(unittest.TestCase):
     """ this tests methods of base
         Atributes:
             All
     """
 
     @classmethod
-    def setUpClass(self):
+    def test_setUpClass(self):
         print("Base setUpClass")
-    
+
     @classmethod
-    def tearDownClass(self):
+    def test_tearDownClass(self):
         print("base tearDownClass")
-    def setUp(self):
+
+    def test_setUp(self):
         print("base setUp")
-    def tearDown(self):
+
+    def test_tearDown(self):
         print("base tearDown")
+
     def test_foo(self):
         self.assertTrue(True)
+    
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py'])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
 
 
 if __name__ == '__main__':
     unittest.main()
-
