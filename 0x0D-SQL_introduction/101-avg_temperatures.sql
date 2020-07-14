bd9dbadc45474044375f9682cd2c7dc696bc10c0
mauricioolarte <reneolarte@yahoo.com>
2020-07-14 05:16:25 +0000

    fix description

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- this display temperatrut
 -- ordered by city and average
-SELECT city, AVG(value)
+SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY AVG(VALUE) DESC;
