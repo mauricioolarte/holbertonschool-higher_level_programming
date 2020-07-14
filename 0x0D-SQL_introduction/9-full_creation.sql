54007c94d35f21a4dec8db28d84515974f376e5b
mauricioolarte <reneolarte@yahoo.com>
2020-07-14 03:36:51 +0000

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- this create and set a table
+-- second table and values
+CREATE TABLE IF NOT EXISTS second_table (id int, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
