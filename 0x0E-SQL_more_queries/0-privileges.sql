33aac3b0c3c69be586664cb4f1d08d1e772ccc11
mauricioolarte <reneolarte@yahoo.com>
2020-07-15 00:47:59 +0000

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- this verify the priveleges of a user.
 -- show my priveleges.
-SHOW GRANTS user_0d_1;
-SHOW GRANTS user_0d_2;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2;localhost;
