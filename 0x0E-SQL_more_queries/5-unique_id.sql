61e4cde305dee16f0190e04f5a2ffe11d50e662a
mauricioolarte <reneolarte@yahoo.com>
2020-07-15 02:09:48 +0000

    5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- This file create a table whit values not nules.
 -- this create a table id_not_null.
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
