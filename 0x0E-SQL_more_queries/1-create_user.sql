ec3ed573cda2cc11a259b66fb52fd44bfa0a305d
mauricioolarte <reneolarte@yahoo.com>
2020-07-15 01:07:44 +0000

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- this is for cration of users whit all priveleges.
+-- this is user1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
