7ab7da273fe7d577c945eba2e663f4624a801fca
mauricioolarte <reneolarte@yahoo.com>
2020-07-16 05:24:07 +0000

    13 fix number of column number_shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,18 @@
 -- this is joinig tables
--- generes id
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, genre_id
+-- generes  From number_of_show=(
+
+SELECT name, COUNT(genre_id) AS number_of_shows
+FROM (SELECT tv_genres.name, tv_show_genres.genre_id
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id) AS geres
+GROUP BY name
+ORDER BY number_of_shows DESC;
+
+
+
+
+/*
+(SELECT tv_genres.name, tv_show_genres.genre_id
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id)
+*/
