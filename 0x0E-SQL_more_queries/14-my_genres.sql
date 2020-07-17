ee6ef5fbfc1c3ae836eeb11e97f3eae9a7e354db
mauricioolarte <reneolarte@yahoo.com>
2020-07-17 03:09:54 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,5 +4,5 @@
 SELECT name
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
-INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.genre_id
-WHERE show_id= 8;
+INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE title = "Dexter";
