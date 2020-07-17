703d5c5395c3d41c20c899b9e09f29b6707b6fbe
mauricioolarte <reneolarte@yahoo.com>
2020-07-17 01:33:33 +0000

    13-count_shows_by_genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- this is joinig tables
 -- generes  From number_of_show=(
 
-SELECT name, count(genre_id) as number_of_shows
+SELECT name as genre, count(genre_id) as number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
 INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.genre_id
-GROUP BY name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
