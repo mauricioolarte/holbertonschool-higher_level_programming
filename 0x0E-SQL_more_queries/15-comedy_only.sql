587b3c47e76fe52611077b1be049bf6129cd5caf
mauricioolarte <reneolarte@yahoo.com>
2020-07-17 03:17:09 +0000

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,9 @@
 -- this is joinig tables
--- generes id
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, genre_id
+-- generes  From number_of_show=(
+
+SELECT title
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
+INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE name = "Comedy"
+ORDER BY title ASC;
