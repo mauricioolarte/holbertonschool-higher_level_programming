ef2d0fe92724d7f8ec6a1899f46fd17b16a4c94f
mauricioolarte <reneolarte@yahoo.com>
2020-07-17 03:14:09 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,9 @@
 -- this is joinig tables
 -- generes  From number_of_show=(
 
-SELECT name
+SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
 INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-WHERE title = "Dexter";
+WHERE title = "Dexter"
+ORDER BY name ASC;
