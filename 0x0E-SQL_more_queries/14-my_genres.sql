b7f90fab321cdd07cf38d8d0bc0abf109178f7b3
mauricioolarte <reneolarte@yahoo.com>
2020-07-17 01:47:14 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,8 @@
 -- this is joinig tables
--- generes id
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, genre_id
+-- generes  From number_of_show=(
+
+SELECT name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
+INNER JOIN tv_shows ON tv_shows.id=tv_show_genres.genre_id
+WHERE show_id= 8;
