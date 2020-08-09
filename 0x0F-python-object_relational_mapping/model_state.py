4eaaf19eb2ec66f276d157409314c18217ab5b81
mauricioolarte <reneolarte@yahoo.com>
2020-08-09 06:10:46 +0000

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 """Start link class to table in database
 """
-import sys
+# import sys
 import sqlalchemy
 
 # from model_state import Base, State
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy import (create_engine)
+# from sqlalchemy import (create_engine)
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                           sys.argv[1], sys.argv[2],
-                           sys.argv[3]), pool_pre_ping=True)
+#    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+#                           sys.argv[1], sys.argv[2],
+#                           sys.argv[3]), pool_pre_ping=True)
 
 #    Base.metadata.create_all(engine)
+
+    conn = engine.connect()
+    conn.execute("SELECT Host FROM INFORMATION_SCHEMA.PROCESSLIST\
+                 WHERE ID = CONNECTION_ID()").fetchall()[('localhost:3306',)]
+
 
     Base = declarative_base()
 
@@ -27,4 +32,4 @@ if __name__ == "__main__":
                     primary_key=True)
         name = Column(String(128), nullable=False)
 
-    Base.metadata.create_all(engine)
+#    Base.metadata.create_all(engine)
