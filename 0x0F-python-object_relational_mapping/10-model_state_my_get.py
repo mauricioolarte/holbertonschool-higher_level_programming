170bf387bc284a75c5a7f0dbda35d78f62e7804a
mauricioolarte <reneolarte@yahoo.com>
2020-08-09 19:50:54 +0000

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy import asc
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).filter_by(name="{}".format(sys.argv[4]))
+    for row in query:
+        if row.id:
+            print("{}".format(row.id))
+        else:
+            print("Not found")
