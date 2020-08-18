a2b948e818f6cd0afaa53cf2409956ac1e8d4e98
mauricioolarte <reneolarte@yahoo.com>
2020-08-18 19:56:32 +0000

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#this show the size of dowload whit curl
-curl $1 -w '%{size_request} bytes\n'
+#this show the size of dowload whit 
+curl -so /dev/null $1 -w '%{size_download}\n'
