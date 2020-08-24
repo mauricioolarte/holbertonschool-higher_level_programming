215b3c84b2aff5d4921b86edfc9f66ff241008ac
mauricioolarte <reneolarte@yahoo.com>
2020-08-24 21:15:34 +0000

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" this module script that fetches https://intranet.hbtn.io/status """
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+
+    except urllib.error.HTTPError as e:
+        print(e.read().decode('utf-8'))
