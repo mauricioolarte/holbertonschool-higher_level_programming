1f1030e379ed354ce7bdd6f0bf4afb5b80bb0fec
mauricioolarte <reneolarte@yahoo.com>
2020-08-24 20:01:37 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         html1 = response.read().decode('utf-8')
 
-    print(("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(type(html), html, html1)))
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(html), html, html1))
