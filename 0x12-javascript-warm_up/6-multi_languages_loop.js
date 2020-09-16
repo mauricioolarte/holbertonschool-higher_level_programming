70060df26b88c8b1242231550566c2232eec0174
mauricioolarte <reneolarte@yahoo.com>
2020-09-15 21:45:06 -0500

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let i;
-const lenguages = ['C is fun', 'Python is coo', 'Javascript is amazing'];
+const lenguages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (i = 0; i < lenguages.length; i++) {
   console.log(lenguages[i]);
 }
