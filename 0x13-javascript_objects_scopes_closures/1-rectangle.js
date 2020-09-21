ac05760b4a22d7d0e3a39d85dce5acd554b6ea3f
mauricioolarte <reneolarte@yahoo.com>
2020-09-21 13:59:29 -0500

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// this is class recngle.
+
+class Rectangle {
+  constructor (height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+module.exports.Rectangle = Rectangle;
