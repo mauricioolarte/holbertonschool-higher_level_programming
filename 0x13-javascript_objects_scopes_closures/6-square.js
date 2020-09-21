7d2de7891be7f0663c219a7b97aa29296391e56e
mauricioolarte <reneolarte@yahoo.com>
2020-09-21 17:55:05 -0500

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// this class heritance of rectangle
+
+const Rectangle = require('./4-rectangle');
+
+let i, stingc;
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c == null) {
+      this.print();
+    } else {
+      stingc = c;
+      for (i = 0; i < this.height; i++) {
+        console.log(stingc.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
