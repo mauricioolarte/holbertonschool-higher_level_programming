4e18f100afd1ba38e3c7a3b114ebcff3f4bdfc43
mauricioolarte <reneolarte@yahoo.com>
2020-09-21 19:24:37 -0500

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 // function that returns the reversed version of a list
 
+let i;
 exports.esrever = function (list) {
-  const newList = list.reverse();
+  const newList = [];
+  for (i = list.length - 1; i >= 0; i--) {
+    newList.splice(0, 0, list[i]);
+  }
   return (newList);
 };
