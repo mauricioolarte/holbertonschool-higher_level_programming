49ad2aa2493b6f14ba8787df4dc8d78da3266136
mauricioolarte <reneolarte@yahoo.com>
2020-09-21 23:23:22 -0500

    10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 // function that converts a number from base 10 to another base passed as argument
-let inputbase = 10;
+
 exports.converter = function (base) {
-    let inputbase = base;
-    the_base_number = base;
-    return(inputbase.toString(the_base_number));
+    return(base.toString(base));
 }
-console.log(this.converter(10))
-console.log(this.converter(2))
