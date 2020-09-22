6552fb8d986a5d497d3e780e62772a92f5bf4286
mauricioolarte <reneolarte@yahoo.com>
2020-09-22 17:58:31 -0500

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,9 @@ const request = require('request');
 
 const urlconsu = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(urlconsu, function (error, response, body) {
-    let resp = JSON.parse(response.body)
-  console.log(resp['title']); // Print the HTML for the Google homepage.
+  if (error) {
+    console.error('error:', error);
+  }
+  const resp = JSON.parse(response.body);
+  console.log(resp.title);
 });
