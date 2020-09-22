c43a11d7fa9c94b76bf5ac4a0395ab865634ad1a
mauricioolarte <reneolarte@yahoo.com>
2020-09-21 23:25:16 -0500

    10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,5 +2,5 @@
 // function that converts a number from base 10 to another base passed as argument
 
 exports.converter = function (base) {
-    return(base.toString(base));
-}
+  return (base.toString(base));
+};
