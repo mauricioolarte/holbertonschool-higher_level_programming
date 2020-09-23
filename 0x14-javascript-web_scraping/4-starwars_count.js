2f027eff6f0b42669a2110ed5b1d8ccf687513a7
mauricioolarte <reneolarte@yahoo.com>
2020-09-22 19:02:04 -0500

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+//  that prints the title of a Star Wars movie where the episode number
+
+const request = require('request');
+
+const urlconsu = 'https://swapi-api.hbtn.io/api/films/';
+request(urlconsu, function (error, response, body) {
+  let count = 0;
+  if (error) {
+    console.error('error:', error);
+  }
+  const resp = JSON.parse(response.body);
+  const results = resp.results;
+  for (const elem in results) {
+    for (const dir in results[elem].characters) {
+      if (results[elem].characters[dir] === 'https://swapi-api.hbtn.io/api/people/18/') {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
