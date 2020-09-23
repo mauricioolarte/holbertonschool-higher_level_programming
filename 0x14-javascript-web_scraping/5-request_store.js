7c6735b675707cb6ff656ea643b07b031b0cd13c
mauricioolarte <reneolarte@yahoo.com>
2020-09-22 21:47:19 -0500

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,32 +2,22 @@
 //  that prints the title of a Star Wars movie where the episode number
 
 const request = require('request');
-const formdata = require('form-data');
 let resp = '';
 const urlconsu = process.argv[2];
 
 request(urlconsu, function (error, response, body) {
   if (error) {
     console.error('error:', error);
+    return;
   }
   resp = (response.body);
   const pathfile = process.argv[3];
   const fs = require('fs');
-  fs.writeFile(pathfile, resp, function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
+  fs.writeFile(pathfile, resp, { encoding: 'utf8' }, function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
   //   console.log(data);
   });
 //   console.log(resp)
-})
-
-
-// const pathfile = process.argv[3];
-// const fs = require('fs');
-// fs.writeFile(pathfile, resp, function (err, data) {
-//   if (err) {
-//     return console.log(err);
-//   }
-// //   console.log(data);
-// });
+});
