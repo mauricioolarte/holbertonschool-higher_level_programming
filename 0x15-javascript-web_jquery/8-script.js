7ff92377bce76241e096fb091936504306657adc
mauricioolarte <reneolarte@yahoo.com>
2020-09-24 19:00:41 -0500

    8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,13 @@
+/*
+script that fetches and lists the title for all movies by using this
+URL: https://swapi-api.hbtn.io/api/films/?format=json
+*/
 let i;
 
-$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
   let film = '';
-  for (i = 0; i < 5; i++) {
-    $.get(data.films[i], function (data) {
+  for (i = 0; i < data.results.length; i++) {
+    $.get(data.results[i], function (data) {
       film = '<li>' + data.title + '</li>';
       $('#list_movies').append(film);
     });
